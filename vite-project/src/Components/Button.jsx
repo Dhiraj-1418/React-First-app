a0e1579adce14dedd794635e8a8efd65a548121b
Dhiraj-1418 <8475dhirajyadav@gmail.com>
2025-06-25 00:07:47 +0530

    update

---
+++ vite-project/src/Components/Button.jsx
@@ -0,0 +1,10 @@
+function FollowButton(){
+    return <button id="btn" className="btn btn-primary ">Follow</button>
+
+    // {
+    //     document.getElementById("btn").addEventListener("click",()=>{
+    //         document.getElementById("btn").innerText="Following"
+    //     })
+    // }
+}
+export default FollowButton;
